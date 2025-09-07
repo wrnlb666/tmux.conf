3c85e0e149d1977b01cf4631aa86b7db2d6b47c2
wrnlb666 <wrnlb666@gmail.com>
2025-09-08 01:31:02 +1000

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 [ -e tmux ] && rm -rf tmux
-git clone git@github.com:gpakosz/.tmux.git tmux
+git clone git@github.com:gpakosz/.tmux.git tmux >/dev/null 2>/dev/null
 
 ln -sf $(pwd)/tmux/.tmux.conf ~/
 ln -sf $(pwd)/.tmux.conf.local ~/
