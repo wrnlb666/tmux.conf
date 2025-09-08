e319d94cd27324462262eca1d36854d453bbf21b
wrnlb666 <wrnlb666@gmail.com>
2025-09-09 00:43:50 +1000

    add update.sh

---
+++ update.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+[ -d tmux ] || git clone git@github.com:gpakosz/.tmux.git tmux >/dev/null 2>/dev/null
+(cd tmux && git checkout master >/dev/null 2>/dev/null && git pull >/dev/null 2>/dev/null)
