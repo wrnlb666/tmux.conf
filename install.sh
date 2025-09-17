d84ce0ce590fb5e525d584312d7359af46c9c587
wrnlb666 <wrnlb666@gmail.com>
2025-09-17 12:25:43 +1000

    rename tmux.conf.local

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@
 git clone git@github.com:gpakosz/.tmux.git tmux >/dev/null 2>/dev/null
 
 ln -sf $(pwd)/tmux/.tmux.conf ~/
-ln -sf $(pwd)/.tmux.conf.local ~/
+ln -sf $(pwd)/tmux.conf.local ~/.tmux.conf.local
