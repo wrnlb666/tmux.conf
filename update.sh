1ae412bc5a6501a75373b7f33f49406952a2065a
wrnlb666 <wrnlb666@gmail.com>
2025-12-07 17:11:24 +1100

    update to use http instead of ssh for git clone

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-[ -d tmux ] || git clone git@github.com:gpakosz/.tmux.git tmux >/dev/null 2>/dev/null
+[ -d tmux ] || git clone https://github.com/gpakosz/.tmux.git tmux >/dev/null 2>/dev/null
 (cd tmux && git checkout master >/dev/null 2>/dev/null && git pull >/dev/null 2>/dev/null)
