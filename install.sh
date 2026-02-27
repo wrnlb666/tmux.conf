745062d572c674263856d2fa32fb86631927537b
wrnlb666 <wrnlb666@gmail.com>
2026-02-28 02:13:50 +1100

    add double quotes

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 [ -e tmux ] && rm -rf tmux
 git clone https://github.com/gpakosz/.tmux.git tmux >/dev/null 2>/dev/null
 
-ln -sf $(pwd)/tmux/.tmux.conf ~/
-ln -sf $(pwd)/tmux.conf.local ~/.tmux.conf.local
+ln -sf "$(pwd)"/tmux/.tmux.conf ~/
+ln -sf "$(pwd)"/tmux.conf.local ~/.tmux.conf.local
